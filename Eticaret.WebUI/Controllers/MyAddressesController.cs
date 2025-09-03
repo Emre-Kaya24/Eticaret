c3aa13f6a1247bd16d491c4b7d35630ebce53f02
EMRE-KAYA <EMRE@DESKTOP-LGO92CF>
2025-09-03 11:13:39 +0300

    Kullanıcı Adres Ekleme İşlemi Tamamlandı.

--- Eticaret.WebUI/Controllers/MyAddressesController.cs
+++ Eticaret.WebUI/Controllers/MyAddressesController.cs
@@ -1,7 +1,9 @@
-﻿using Eticaret.Core.Entities;
+﻿    using Eticaret.Core.Entities;
 using Eticaret.Service.Abstract;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace Eticaret.WebUI.Controllers
@@ -26,6 +28,40 @@ namespace Eticaret.WebUI.Controllers
             }
             var model = await _serviceAddress.GetAllAsync(u => u.AppUserId == appuser.Id);
             return View(model);
+        }
+        public IActionResult Create()
+        {
+           
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Address address)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var appuser = await _serviceAppUser.GetAsync(x => x.UserGuid.ToString() == HttpContext.User.FindFirst("UserGuid").Value);
+                    if(appuser != null)
+                    {
+                        address.AppUserId = appuser.Id;
+                        _serviceAddress.Add(address);
+                        await _serviceAddress.SaveChangesAsync();
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+                catch (Exception)
+                {
+
+                    ModelState.AddModelError("", "Adres Eklenirken Bir Hata Oluştu! Lütfen Tüm Alanları Kontrol Edip Tekrar Deneyiniz.");
+                }
+                
+            }
+            ModelState.AddModelError("", "Kayıt Başarısız!");
+            return View(address);
         }
     }
 }
