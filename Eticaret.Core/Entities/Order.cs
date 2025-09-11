cb8bb3e5c35f851d6033204977547ecc805cb2e0
EMRE-KAYA <EMRE@DESKTOP-LGO92CF>
2025-09-11 22:58:33 +0300

    Sipariş durumu MyOrders sayfasında gösterildi

--- Eticaret.Core/Entities/Order.cs
+++ Eticaret.Core/Entities/Order.cs
@@ -23,6 +23,7 @@ namespace Eticaret.Core.Entities
         [Display(Name = "Müşteri")]
         public AppUser? AppUser { get; set; }
 
+        [Display(Name = "Sipariş Durumu")]
         public EnumOrderState OrderState { get; set; }
 
     }
