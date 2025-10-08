b9ababe482812f8258746d4b419f480246c3d8f6
EMRE-KAYA <EMRE@DESKTOP-LGO92CF>
2025-10-08 20:13:43 +0300

    Iyzico Sepet Öğelerini Dinamikleştirme İşlemi Tamamlandı.

--- Eticaret.WebUI/Controllers/CartController.cs
+++ Eticaret.WebUI/Controllers/CartController.cs
@@ -151,8 +151,8 @@ namespace Eticaret.WebUI.Controllers
             CreatePaymentRequest request = new CreatePaymentRequest();
             request.Locale = Locale.TR.ToString();
             request.ConversationId = HttpContext.Session.Id;
-            request.Price = siparis.TotalPrice.ToString();
-            request.PaidPrice = siparis.TotalPrice.ToString();
+            request.Price = siparis.TotalPrice.ToString().Replace(",", ".");
+            request.PaidPrice = siparis.TotalPrice.ToString().Replace(",", ".");
             request.Currency = Currency.TRY.ToString();
             request.Installment = 1;
             request.BasketId = "B"+HttpContext.Session.Id;
@@ -176,8 +176,8 @@ namespace Eticaret.WebUI.Controllers
             buyer.GsmNumber = appuser.Phone;
             buyer.Email = appuser.Email;
             buyer.IdentityNumber = "11111111111";
-            buyer.LastLoginDate = DateTime.Now.ToString(); //"2015-10-05 12:43:35";
-            buyer.RegistrationDate = appuser.CreateDate.ToString(); //"2013-04-21 15:12:09";
+            buyer.LastLoginDate = DateTime.Now.ToString("yyyy-mm-dd hh:mm:ss"); //"2015-10-05 12:43:35";
+            buyer.RegistrationDate = appuser.CreateDate.ToString("yyyy-mm-dd hh:mm:ss"); //"2013-04-21 15:12:09";
             buyer.RegistrationAddress = siparis.DeliveryAddress;
             buyer.Ip = HttpContext.Connection.RemoteIpAddress?.ToString(); //"85.34.78.112";
             buyer.City = TeslimatAdresi.City;
@@ -202,33 +202,29 @@ namespace Eticaret.WebUI.Controllers
             request.BillingAddress = billingAddress;
 
             List<BasketItem> basketItems = new List<BasketItem>();
-            BasketItem firstBasketItem = new BasketItem();
-            firstBasketItem.Id = "BI101";
-            firstBasketItem.Name = "Binocular";
-            firstBasketItem.Category1 = "Collectibles";
-            firstBasketItem.Category2 = "Accessories";
-            firstBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
-            firstBasketItem.Price = "0.3";
-            basketItems.Add(firstBasketItem);
 
-            BasketItem secondBasketItem = new BasketItem();
-            secondBasketItem.Id = "BI102";
-            secondBasketItem.Name = "Game code";
-            secondBasketItem.Category1 = "Game";
-            secondBasketItem.Category2 = "Online Game Items";
-            secondBasketItem.ItemType = BasketItemType.VIRTUAL.ToString();
-            secondBasketItem.Price = "0.5";
-            basketItems.Add(secondBasketItem);
+            //BasketItem firstBasketItem = new BasketItem();
+            //firstBasketItem.Id = "BI101";
+            //firstBasketItem.Name = "Binocular";
+            //firstBasketItem.Category1 = "Collectibles";
+            //firstBasketItem.Category2 = "Accessories";
+            //firstBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
+            //firstBasketItem.Price = "0.3";
+            //basketItems.Add(firstBasketItem);
 
-            BasketItem thirdBasketItem = new BasketItem();
-            thirdBasketItem.Id = "BI103";
-            thirdBasketItem.Name = "Usb";
-            thirdBasketItem.Category1 = "Electronics";
-            thirdBasketItem.Category2 = "Usb / Cable";
-            thirdBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
-            thirdBasketItem.Price = "0.2";
-            basketItems.Add(thirdBasketItem);
-            request.BasketItems = basketItems;
+            foreach (var item in cart.CartLines)
+            {
+                basketItems.Add(new BasketItem
+                {
+                    Id = item.Product.Id.ToString(),
+                    Name = item.Product.Name,
+                    Category1 = "Collectibles",
+                    ItemType = BasketItemType.PHYSICAL.ToString(),
+                    Price = item.Product.Price.ToString().Replace(",",".")
+                });
+            }
+
+                request.BasketItems = basketItems;
 
             Payment payment = await Payment.Create(request, options);
             
@@ -253,12 +249,12 @@ namespace Eticaret.WebUI.Controllers
                 
                 else
                 {
-                    TempData["Message"] = "Ödeme İşlemi Başarısız!";
+                    TempData["Message"] = "<div class='alert alert-danger'>Ödeme İşlemi Başarısız!</div>";
                 }
             }
             catch (Exception ex)
             {
-                TempData["Message"] = "Hata Oluştu";
+                TempData["Message"] = "<div class='alert alert-danger'>Hata Oluştu</div>";
             }
 
             return View(model);
